5546fed0bd7b5a20d99de983eff5e4b2ad5c5657
Henrique Starosky <hgs.true@gmail.com>
2018-06-26 17:32:46 -0300

    Agora com suporte ao BD 😄

--- api/src/routes/db.ts
+++ api/src/routes/db.ts
@@ -2,13 +2,22 @@ import * as koa from 'koa'
 import * as Router from 'koa-router'
 import { PessoaFisica } from '../../../classes/pessoa-fisica.class'
 import { db } from '../server'
+import { emit } from 'cluster';
 
 const router = new Router({ prefix: '/api' });
 
-router.get('/getLista', async (ctx: koa.Context) => {
-    db.all('SELECT cpf, nome, email FROM cpfs', (ctx) => {
-
+router.get('/getLista', async (ctx) => {
+    let respostas = new Array<PessoaFisica>();
+    await new Promise((resolve, reject) => {
+        db.each('SELECT cpf, nome, email FROM cpfs', (err, row) => {
+            respostas.push(new PessoaFisica(row.cpf, row.nome, row.email));
+        }, (err) => {
+            if (err) console.log(err)
+            else resolve();
+        });
     });
+    ctx.body = JSON.stringify(respostas);
+    console.log('Depois da Query');
 })
 
 /*router.get('/*', async (ctx) => {
